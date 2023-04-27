7ef276ad3286af3043970b2895c33b326717109b
Kunzhe Song <74352592+kz-song@users.noreply.github.com>
2023-04-27 21:12:24 +0800

    Update download.sh

--- weights/clip/download.sh
+++ weights/clip/download.sh
@@ -1,6 +1,6 @@
 pip install git+https://github.com/openai/CLIP.git
 
-wget https://openaipublic.azureedge.net/clip/models/40d365715913c9da98579312b702a82c18be219cc2a73407c4526f58eba950af/ViT-B-32.pt
+wget -P ./weights/clip/ https://openaipublic.azureedge.net/clip/models/40d365715913c9da98579312b702a82c18be219cc2a73407c4526f58eba950af/ViT-B-32.pt
 
 
 
